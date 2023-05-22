cd2e8ed5d9563002113bed58a628057122e2ed64
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-05-22 13:45:22 +0300

    Some work on fonoteca

--- Laborator 10/fonoteca/fonoteca.js
+++ Laborator 10/fonoteca/fonoteca.js
@@ -1,5 +1,34 @@
-window.onload = function() {
-   
-   /* ... */
-}
-
+(async () => {
+  let albums = await fetch("albums.json").then(response => response.json());
+  let gallery = document.getElementById("gallery");
+  for(let album of albums) {
+    console.log(album);
+    let div = document.createElement("a");
+    div.classList.add("album-cover");
+    div.setAttribute("tabindex", '0');
+    gallery.appendChild(div);
+    let text = document.createElement("p");
+    text.classList.add("title");
+    div.appendChild(text);
+    let title = document.createElement("span");
+    text.appendChild(title);
+    title.classList.add("name");
+    title.innerText = album.name;
+    let by = document.createElement("span");
+    by.classList.add("by");
+    text.appendChild(by);
+    by.innerText = " by " + album.artist;
+    let cover = document.createElement("img");
+    cover.setAttribute("src", "images/" + album.image);
+    div.appendChild(cover);
+    function startLoad(album) {
+      alert(album.innerHTML);
+    }
+    div.addEventListener("click", e => startLoad(e.currentTarget));
+    div.addEventListener("keypress", e => {
+      if(e.key === "Enter") {
+        startLoad(e.currentTarget);
+      }
+    });
+  }
+})();
